cea79ccb88c733f5f39d5e5d35759d7b2982e231
Arthur <arthurbm15@gmail.com>
2024-05-31 12:02:59 -0300

    Adding multiples sum numbers tests

--- cypress/e2e/e2e_example.cy.js
+++ cypress/e2e/e2e_example.cy.js
@@ -18,12 +18,24 @@ Cypress.Commands.add('checkResult', (expectedResult) => {
     cy.get('#addmessage').should('have.text', expectedResult);
 });
 
-describe('My Cypress Test', () => {
-    it('should perform addition', () => {
-        cy.accessWebsite('https://www.lambdatest.com/selenium-playground/simple-form-demo');
-        cy.enterNumberInFirstField(5);
-        cy.enterNumberInSecondField(10);
+describe('Sum Form Test', () => {
+    const testCases = [
+        { first_number: '5', second_number: '10', expected_result: '15' },
+        { first_number: '3', second_number: '7', expected_result: '10' },
+        { first_number: '1', second_number: '1', expected_result: '2' },
+        { first_number: '13', second_number: '1', expected_result: '14' },
+        { first_number: '999', second_number: '1', expected_result: '1000' },
+    ];
+
+    testCases.forEach((testCase) => {
+        it(`should add ${testCase.first_number} and ${testCase.second_number} to get ${testCase.expected_result}`, () => {
+            // Access the website
+            cy.accessWebsite('https://www.lambdatest.com/selenium-playground/simple-form-demo');
+
+            cy.enterNumberInFirstField(testCase.first_number);
+        cy.enterNumberInSecondField(testCase.second_number);
         cy.clickButton('Get Sum');
-        cy.checkResult('15');
+        cy.checkResult(testCase.expected_result);
+        });
     });
 });
